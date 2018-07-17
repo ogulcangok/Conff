8641b46b5292c3511d5e09a506570ca4046db580
selimkundakci <selim.kundakcioglu@gmail.com>
2018-07-17 11:11:01 +0300

    Ufak düzenlemeler yapıldı

--- event-detail.php
+++ event-detail.php
@@ -290,7 +290,6 @@ print_r($get_hall_number);
             <div style="width: 100%; height: 350px; border: 1px solid #707070; text-align: center">Reklam Alanı</div>
         </div>
         <br>
-        ---------------------------------------------------------------------------------------------------------------------------------------
         <div class="col-md-7 mt10">
             <h3 style="font-weight: bold; color: #444"><?php
                 echo $event['event_title'];
@@ -500,126 +499,7 @@ print_r($get_hall_number);
 
             </div>
 
-            <h3 style="font-weight: bold; color: #444; margin-top: 60px; margin-bottom:20px; border-bottom: 1px solid #dd153c; padding-bottom: 15px;">
-                S.S.S
-            </h3>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="panel-group" id="accordion" role="tablist"
-                         aria-multiselectable="true">
-                        <div class="panel panel-default brdr">
-                            <div class="panel-heading" style="background: #0000" role="tab" id="headingOne">
-                                <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion"
-                                       href="#collapseOne" aria-expanded="true"
-                                       aria-controls="collapseOne">
-                                        <div class="row">
-                                            <div class="col-md-10 col-sm-11 col-xs-10">
-                                                Yeni zirve nasıl oluşturabilirim?
-                                            </div>
-                                            <div class="col-md-2 col-sm-1 col-xs-2"
-                                                 style="height:100%; border-left: 1px solid #ddd; ">
-                                                <i class="fa fa-plus" style="font-size: 20px;"></i>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </h4>
-                            </div>
-                            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
-                                 aria-labelledby="headingOne">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                    accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                    non cupidatat skateboard dolor brunch. Food truck quinoa
-                                    nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                                    put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                    wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                    excepteur butcher vice lomo. Leggings occaecat craft beer
-                                    farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                    haven't heard of them accusamus labore sustainable VHS.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-group" id="accordion2" role="tablist"
-                         aria-multiselectable="true">
-                        <div class="panel panel-default brdr">
-                            <div class="panel-heading" style="background: #0000" role="tab" id="headingTwo">
-                                <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion2"
-                                       href="#collapseTwo" aria-expanded="true"
-                                       aria-controls="collapseTwo">
-                                        <div class="row">
-                                            <div class="col-md-10 col-sm-11 col-xs-10">
-                                                Yeni zirve nasıl oluşturabilirim?
-                                            </div>
-                                            <div class="col-md-2 col-sm-1 col-xs-2"
-                                                 style="height:100%; border-left: 1px solid #ddd;">
-                                                <i class="fa fa-plus" style="font-size: 20px;"></i>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </h4>
-                            </div>
-                            <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel"
-                                 aria-labelledby="headingTwo">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                    accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                    non cupidatat skateboard dolor brunch. Food truck quinoa
-                                    nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                                    put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                    wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                    excepteur butcher vice lomo. Leggings occaecat craft beer
-                                    farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                    haven't heard of them accusamus labore sustainable VHS.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="panel-group" id="accordion3" role="tablist"
-                         aria-multiselectable="true">
-                        <div class="panel panel-default brdr">
-                            <div class="panel-heading" style="background: #0000" role="tab" id="headingThree">
-                                <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion3"
-                                       href="#collapseThree" aria-expanded="true"
-                                       aria-controls="collapseThree">
-                                        <div class="row">
-                                            <div class="col-md-10 col-sm-11 col-xs-10">
-                                                Yeni zirve nasıl oluşturabilirim?
-                                            </div>
-                                            <div class="col-md-2 col-sm-1 col-xs-2"
-                                                 style="height:100%; border-left: 1px solid #ddd;">
-                                                <i class="fa fa-plus" style="font-size: 20px;"></i>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </h4>
-                            </div>
-                            <div id="collapseThree" class="panel-collapse collapse in" role="tabpanel"
-                                 aria-labelledby="headingThree">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                    accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                    non cupidatat skateboard dolor brunch. Food truck quinoa
-                                    nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                                    put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                    wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                                    excepteur butcher vice lomo. Leggings occaecat craft beer
-                                    farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                    haven't heard of them accusamus labore sustainable VHS.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!--     S.S.S       -->
         </div>
         <div class="col-md-2 mt10" style="padding-left: 0; padding-right: 0;">
             <div class="btm-mrg-40">
